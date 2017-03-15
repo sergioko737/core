8158493d451aac75ee59a94980ae5142c47832e2
sergioko737 <s29585476@gmail.com>
2017-03-15 22:44:50 +0200

    Add form2

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,9 @@
-    from django import forms
-    from .models import Person
+from django import forms
+from captcha.fields import CaptchaField
+from .models import Person
 
-    class PersonForm(forms.ModelForm):
-        captcha = CaptchaField()
-        class Meta:
-            model = Person
-            fields = ('first_name', 'last_name', 'email',)
+class PersonForm(forms.ModelForm):
+    captcha = CaptchaField()
+    class Meta:
+        model = Person
+        fields = ('first_name', 'last_name', 'email',)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Person
+from .forms import PersonForm
 
 # Create your views here.
 def reg_form(request):
-    return render(request, 'core/reg_form.html', {})
+    if request.method == "POST":
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.published_date = timezone.now()
+            post.save()
+            return render(request, 'core/reg_form.html', {'form': form})
+    else:
+        form = PersonForm()
+    return render(request, 'core/reg_form.html', {'form': form})
